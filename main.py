82d8b3c8440e0e5e4300a914217777c28854c04c
pishchenyuk <72612741+pishchenyuk@users.noreply.github.com>
2024-06-03 01:38:42 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -13,21 +13,24 @@ def bot_login():
     return r
 
 
+# Запуск бота
 def run_bot(r):
     cur = database.con.cursor()
-    print("Происходит проверка 50 комментариев")
-    for comment in r.subreddit('rusAskReddit').comments(limit=50):
-        if (("университет" in comment.body.lower() or "уник" in comment.body.lower() or "вуз" in comment.body.lower())
-                and comment.author != r.user.me() and len(cur.execute(
-                f"SELECT comment_id FROM Comments WHERE comment_id = '{comment.id}'").fetchall()) == 0):
-            print(f"Найден новый комментарий {comment.id}!")
-            comment_reply = ("Дружище, не совершай ошибок, поступай в Московский Политехнический Университет!")
-            comment.reply(comment_reply)
-            print(f"Ответили на комментарий {comment.id}")
-            cur.execute(
-                f"INSERT INTO Comments (comment_id, body, author) VALUES (?, ?, ?)", (comment.id, comment.body, comment.author.name))
-            database.con.commit()
-            time.sleep(10)
+    print("Происходит проверка комментариев")
+    posts = r.subreddit('rusAskReddit').search('универ', sort = "new", time_filter = "month")
+    for post in posts:
+        comments = post.comments.list()
+        for comment in comments:
+            if ("университет" or "универ") in comment.body.lower() and comment.author != r.user.me() and len(cur.execute(
+                    f"SELECT comment_id FROM Comments WHERE comment_id = '{comment.id}'").fetchall()) == 0:
+                print(f"Найден новый комментарий {comment.id, comment.body}!")
+                comment_reply = ("Дружище, не совершай ошибок, поступай в Московский Политехнический Университет!")
+                comment.reply(comment_reply)
+                print(f"Ответили на комментарий {comment.id}")
+                cur.execute(
+                    f"INSERT INTO Comments (comment_id, body, author) VALUES (?, ?, ?)", (comment.id, comment.body, comment.author.name))
+                database.con.commit()
+                time.sleep(610)
 
 
 if __name__ == '__main__':
